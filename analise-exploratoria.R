1a8c3ce88a7b7a7bbddd97dee2422fe57dacbc5c
Fracs97 <fracassobruno@gmail.com>
2021-11-29 11:30:33 -0300

    Checkpoint da função de teste das variáveis

--- analise-exploratoria.R
+++ analise-exploratoria.R
@@ -1,5 +1,23 @@
 library(data.table)
+library(dplyr)
 dados = fread(file='train_sample.csv')
 
 head(dados)
-?fread
+
+#A coluna ip não é útil
+dados[,'ip'] = NULL
+
+#Verificando os tipos dos dados
+str(dados)
+
+#Todos os inteiros são na verdade fatores
+dados = dados %>% mutate_if(is.integer,as.factor)
+
+#Analisando cada variável, separando por classe
+testa_var = function(variavel){
+  sprintf('Verificando a variável %s',variavel)
+  tapply(dados[,variavel],dados$is_attributed,function(x){prop.table(table(x))})
+  chisq.test(dados[,variavel],dados$is_attributed)
+  }
+
+
